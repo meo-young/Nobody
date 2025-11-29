e7ca234d761d3d337fd7aa2fd79eb87a7c9d3d76
meo-young <meoyoung000@naver.com>
2025-11-29 22:12:06 +0900

    Initial commit

---
+++ Source/Nobody/Nobody.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Nobody.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Nobody, "Nobody" );
